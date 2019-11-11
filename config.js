12447832578438ca4fe252d2a5f49ea95185ea36
jschaenz <52198178+jschaenz@users.noreply.github.com>
2019-11-11 10:50:07 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-﻿const oauth = 'oauth:cn7v8azogsclgv7ls4n3bpfvhh5qf8'
-const supiniactive = 'https://supinic.com/api/bot/active?auth_user=2602897&auth_key=03d5385b2f938e6b1df122b1eefcd79a718c27a9fae6423aeea2f7cf3a0083eb'
+const oauth = 'oauth:key'
+const supiniactive = 'https://supinic.com/api/bot/active?auth_user=XXXXXXX&auth_key=XXXXXXX'
 const activechannles = ['manateeoverlord69', 'supinic']
 
 module.exports = { oauth, supiniactive, activechannles}
