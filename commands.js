a2e1e37f14a8b440bc477698fd68b0459c42bed7
jschaenz <52198178+jschaenz@users.noreply.github.com>
2020-02-09 20:53:49 +0100

    "commands" now dynamic

--- commands.js
+++ commands.js
@@ -1,15 +1,46 @@
 const life = require('./life.json');
-const client = require('./bot.js');
+const client = require('./app.js');
 const fetch = require("node-fetch")
 const prefix = "]";
+
+
+function Format(second) {
+    var hour = Math.floor(second / 3600);
+    var minute = Math.floor(second % 3600 / 60);
+    var second = Math.floor(second % 60);
+
+    if (hour === 0 && minute != 0) {
+        return minute + 'm';
+    } else {
+        if (second === 0 || hour === 0 && minute === 0) {
+            return '<1 second'
+        }
+        else if (minute === 0 && hour === 0) {
+            return second + "s"
+        }
+        else {
+            return hour + ' h'
+        }
+    }
+}
+
 
 const commands = [
     {
         name: prefix + "commands",
         invocation: async (text, senderUID) => {
-            return ("ping, bot, bots, eval, calcdeath");
+           
+            const trackObj = commands.filter(
+                i => i.name
+            );
+            const comName = trackObj.map(
+                i => i.name
+            );
 
-            //todo: dynamic
+            const comNumPre = ((comName.sort().toString().replace(/,/g, " | ").replace(/]/g, '') + " |").split('|')).length;
+            const comNum = comNumPre - 1
+
+            return "There are " + comNum + " Commands in total:  " + comName.sort().toString().replace(/,/g, " | ").replace(/]/g, '');
         }
     },
 
@@ -17,13 +48,16 @@ const commands = [
     {
         name: prefix + "ping",
         invocation: async (text, senderUID) => {
-            return ("currently under construction")
-            //todo: make work Okayga
+
             const t0 = process.hrtime();
             await client.client.ping();
             const t1 = process.hrtime(t0);
-            return (t1 - t0).toFixed();
+            const latency = t1[0] * 1e3 + t1[1] / 1e6;
 
+            const uptime = process.uptime();
+            const version = process.version;
+            return (String("manateebot69v2, Node " + version + " uptime: " + Format(uptime) + " Ping: " + latency.tofixed()));
+            //todo: make work Okayga
         }
     },
 
@@ -51,30 +85,9 @@ const commands = [
         invocation: async (text, senderUID) => {
             const time = await fetch("https://supinic.com/api/bot/active").then(response => response.json());
 
-            function Format(second) { 
-                var hour = Math.floor(second / 3600);
-                var minute = Math.floor(second % 3600 / 60);
-                var second = Math.floor(second % 60);
-
-                if (hour === 0 && minute != 0) {
-                    return minute + 'm';
-                } else {
-                    if (second === 0 || hour === 0 && minute === 0) {
-                        return '<1 second ago'
-                    }
-                    else if (minute === 0 && hour === 0) {
-                        return second + "s"
-                    }
-                    else {
-                        return hour + ' h'
-                    }
-                }
-            }
-
             if (time.data.filter(i => i.lastSeenTimestamp != null)) {
                 const activebots = time.data.filter(i => i.lastSeenTimestamp != null).map(
                     i => ' | ' + i.name + ' ' + Format((Math.abs(new Date() - new Date(i.lastSeenTimestamp))) / 1000));
-
                 return (String(activebots));
             }
         }
