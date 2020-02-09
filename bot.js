641560ef6ed1ebc26652874f6020b9ce636e8539
jschaenz <52198178+jschaenz@users.noreply.github.com>
2020-02-09 17:32:48 +0100

    now using dank-twitch-irc and moved commands

--- bot.js
+++ bot.js
@@ -1,12 +1,13 @@
-const fetch = require("node-fetch");
-const lifeExpectancy = require('life-expectancy');
-const tmi = require('tmi.js');
-const dankList = require('./config.js')
-const cooldownList = new Set();
-const mincooldown = 1000;
+//const fetch = require("node-fetch")
+const configs = require('./config.js');
 const pingTime = 3600000;
 const pingAmount = [];
 var lastMessage = null;
+const cmds = require('./commands.js');
+const { ChatClient } = require("dank-twitch-irc");
+
+
+
 
 function sendMsg(channel, message) {
     if (message === lastMessage) {
@@ -18,286 +19,55 @@ function sendMsg(channel, message) {
 
 async function sendOnlineStatus() {
     pingAmount.push('ping')
-    const ping = (await fetch(dankList.supiniactive, {
+    const ping = (await fetch(configs.supiniactive, {
         method: 'PUT',
     }).then(response => response.json()))
     console.log(ping)
 }
 setInterval(() => { sendOnlineStatus() }, pingTime);
 
-const options = {
-    options: {
-        debug: true,
-    },
-    connection: {
-        cluster: 'aws',
-        reconnect: true,
-    },
-    identity: {
-        username: 'manateebot69',
-        password: dankList.oauth,
-    },
-    channels: dankList.activechannles
-};
-const client = new tmi.client(options);
+let client = new ChatClient({
+    username: "manateebot69",
+    password: configs.pw,
+    rateLimits: "default",
+});
+
+client.on("ready", () => console.log("Successfully connected to chat"));
+client.on("close", error => {
+    if (error != null) {
+        console.error("Client closed due to error", error);
+    }
+});
 client.connect();
+client.joinAll(configs.activechannles);
 
-client.on('chat', async (channel, user, message, self) => {
 
-    if (user['display-name'] === "manateeoverlord69") {
-        var permission = 1;
-    }
-    else permission = 0;
 
-    if (message.startsWith("]bot")) {
-        try {
-            if (cooldownList.has(user['user-id'])) {
-            }
-            else {
-                cooldownList.add(user['user-id']);
-                setTimeout(() => {cooldownList.delete(user['user-id']);
-                },5000);
-                
-                sendMsg(channel, 'really basic bot made by manateeoverlord69 using nodejs. Do ]commands for more info');
-            }
-        }
-        catch (err) {
-            sendMsg(channel, user['display-name'] + ' You did a fucky wucky! ' + err);
-        }
+
+
+client.on("PRIVMSG", msg => {
+    console.log(`[#${msg.channelName}] ${msg.senderUserID} ${msg.displayName}: ${msg.messageText}`);
+
+    let message = msg.messageText.split(' ');
+    var i;
+    var text = '';
+    for (i = 1; i < message.length; i++) {
+        text += message[i];
+        text += " ";
     }
 
-
-
-    if (message.startsWith("]commands")) {
-        try {
-
-            if (cooldownList.has(user['user-id'])) {
-            }
-            else {
-                cooldownList.add(user['user-id']);
-                setTimeout(() => {cooldownList.delete(user['user-id']);
-                }, 5000);
-
-                sendMsg(channel, 'commands: bot, ping, pingme, eval, say, calcdeath, verifiedbots');
-            }
+    cmds.commands.forEach(async command => {
+        if (message[0] === command.name) {
+            let result = await command.invocation(text, msg.senderUserID);
+            console.log(result);
+            sendMsg(msg.channelName, result);
         }
-        catch (err) {
-            sendMsg(channel, user['display-name'] + ' You did a fucky wucky! ' + err);
-        }
-    }
+    })
+
+});
 
 
-
-    if (message==="]ping") {
-        try {
-            if (cooldownList.has(user['user-id'])) {
-            }
-            else {
-                cooldownList.add(user['user-id']);
-                setTimeout(() => {cooldownList.delete(user['user-id']);
-                }, 5000);
-
-                client.ping().then(function (data) {
-                    let ping = Math.floor(Math.round(data * 1000))
-                    sendMsg(channel, 'Ping to tmi is ' + ping + 'ms. Last update: November 9th');
-                })
-            }
-        }
-        catch (err) {
-            sendMsg(channel, user['display-name'] + ' You did a fucky wucky! ' + err);
-        }
-    }
-
-
-
-    if (message.startsWith("]pingme")) {
-        try {
-            if (cooldownList.has(user['user-id'])) {
-            }
-            else {
-                cooldownList.add(user['user-id']);
-                setTimeout(() => {cooldownList.delete(user['user-id']);
-                }, 5000);
-
-                sendMsg(channel, user['display-name'] + ' pinged');
-            }
-        }
-        catch (err) {
-            sendMsg(channel, user['display-name'] + ' You did a fucky wucky! ' + err);
-        }
-    }
-
-
-
-    if (message.startsWith("]reservedforfuturecommand")) {
-        try {
-            if (cooldownList.has(user['user-id'])) {
-            }
-            else {
-                cooldownList.add(user['user-id']);
-                setTimeout(() => {cooldownList.delete(user['user-id']);
-                }, 5000);
-
-                sendMsg(channel, user['display-name'] + ' ' );
-            }
-        }
-        catch (err) {
-            sendMsg(channel, user['display-name'] + ' You did a fucky wucky! ' + err);
-        }
-    }
-
-
-
-    if (message.startsWith("]verifiedbots")) {
-        try {
-            const time = await fetch("https://supinic.com/api/bot/active").then(response => response.json());
-    
-            if (cooldownList.has(user['user-id'])) {
-            }
-            else {
-                cooldownList.add(user['user-id']);
-                setTimeout(() => {cooldownList.delete(user['user-id']);
-                }, 5000);
-
-                function twFormat(second) {
-                    var hour = Math.floor(second / 3600);
-                    var minute = Math.floor(second % 3600 / 60);
-                    var second = Math.floor(second % 60);
-
-                    if (hour === 0 && minute != 0) {
-                        return minute + 'm';
-                    } else {
-                        if (second === 0 || hour === 0 && minute === 0) {
-                            return '<1 second ago'
-                        }
-                        else if (minute === 0 && hour === 0) {
-                            return second + "s"
-                        }
-                        else {
-                            return hour + ' h'
-                        }
-                    }
-                } 
-                if (time.data.filter(i => i.lastSeenTimestamp != null)) {
-                    const activebots = time.data.filter(i => i.lastSeenTimestamp != null).map(
-                        i => ' | ' + i.name + ' ' + twFormat((Math.abs(new Date() - new Date(i.lastSeenTimestamp))) / 1000));
-
-                    sendMsg(channel, user['username'] + ', bots reported to API: ' + activebots );
-                 }
-            }
-        }
-        catch (err) {
-            sendMsg(channel, user['display-name'] + ' You did a fucky wucky!' + err);
-        }
-    }
-
-
-
-    if (message.startsWith("]eval")) {
-        try {
-            if (cooldownList.has(user['user-id'])) { }
-            else {
-                cooldownList.add(user['user-id']);
-                setTimeout(() => {cooldownList.delete(user['user-id']);
-                }, mincooldown);
-            
-                if (permission === 1) {
-
-                    const msg = message.split(' ');
-                    var i;
-                    var text = '';
-                    for (i = 1; i < msg.length; i++) {
-                        text += msg[i];
-                        text += ' ';
-                    }
-                    if (message === "]eval return (typeof kunszg)") {
-                        sendMsg(channel, "Racist");
-                    }
-                    else {
-                        const evaluation = await eval('(async () => {' + text + '})()');
-                        console.log(evaluation)
-                        sendMsg(channel, String(evaluation));
-                    }
-                }
-                else if (user['display-name'] != "manateebot69") {
-                    sendMsg(channel, "You dont have permission to use this command! Maybe ask my owner for help FeelsDankMan")
-                }
-            }
-        }
-        catch (err) {
-            sendMsg(channel, user['display-name'] + ' You did a fucky wucky! ' + err);
-        }
-    }
-
-
-
-    if (message.startsWith("]say")) {
-        try {
-            if (cooldownList.has(user['user-id'])) { }
-            else {
-                cooldownList.add(user['user-id']);
-                setTimeout(() => {cooldownList.delete(user['user-id']);
-                }, 1000);
-
-                if (permission === 1) {
-
-                    const msg = message.split(' ');
-                    var i;
-                    var text = '';
-                    for (i = 1; i < msg.length; i++) {
-                        text += msg[i];
-                        text += ' ';
-                    }
-
-                    sendMsg(channel, text);
-                }
-
-                else if (user['display-name'] != "manateebot69") {
-                    sendMsg(channel, "You dont have permission to use this command! Maybe ask my owner for help FeelsDankMan")
-                }
-            }
-        }
-        catch (err) {
-            sendMsg(channel, user['display-name'] + ' You did a fucky wucky! ' + err);
-        }
-    }
-
-
-
-    if (message.startsWith("]calcdeath")) {
-    try {
-        if (cooldownList.has(user['user-id'])) { }
-        else {
-            cooldownList.add(user['user-id']);
-            setTimeout(() => {cooldownList.delete(user['user-id']);
-            }, 5000);
-
-            if (message === "]calcdeath") {
-                sendMsg(channel, 'Use ]calcdeath Country Age');
-            }
-            else {
-                const msg = message.split(' ');
-                var country = msg[1];
-                var age = msg[2];
-                const A = lifeExpectancy(country);
-                var lifeleft = Math.round(A[0].life - age);
-                var deathdate = Math.round(lifeleft + 2019);
-                if (lifeleft < 0) {
-                    lifeleft = lifeleft * -1;
-                    sendMsg(channel, user['display-name'] + ' By the avg. Life expectancy you would have died ' + lifeleft + ' years ago! That was in ' + deathdate);
-                }
-                else {
-                    sendMsg(channel, user['display-name'] + ' you have around ' + lifeleft + ' years of life left, meaning you will most likely die in ' + deathdate
-                    );
-                }
-            }
-        }
-    }
-    catch (err) {
-        sendMsg(channel, user['display-name'] + ' ' + err + ' (Probably wrong country code FeelsDankMan )');
-    }
-}
-
-
-
+client.on("WHISPER", msg => {
+    console.log(`WHISPER: ${msg.displayName}: ${msg.messageText}`);
+  //dont even work cause not verified ome
 });
