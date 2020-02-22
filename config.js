641f72af85d22c05302138453417a074046a149f
jschaenz <52198178+jschaenz@users.noreply.github.com>
2020-02-22 12:35:11 +0100

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-const oauth = 'oauth:key'
-const supiniactive = 'https://supinic.com/api/bot/active?auth_user=XXXXXXX&auth_key=XXXXXXX'
-const activechannles = ['manateeoverlord69', 'supinic']
-
-module.exports = { oauth, supiniactive, activechannles}
